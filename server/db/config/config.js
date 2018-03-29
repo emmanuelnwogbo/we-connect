5b6d6b5fcd840be454d2c4d997ede37db944ba5c
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2018-03-29 17:13:35 +0100

    moved all values to .env file

--- server/db/config/config.js
+++ server/db/config/config.js
@@ -6,19 +6,19 @@ module.exports = {
   development: {
     use_env_variable: "DEV_DB",
     use: {
-      dialect: 'postgres'
+      dialect: 'DB_DIALECT'
     }
   },
   test: {
     use_env_variable: "TEST_DB",
     use: {
-      dialect: 'postgres'
+      dialect: 'DB_DIALECT'
     }
   },
   production: {
     use_env_variable: "PROD_DB",
     use: {
-      dialect: 'postgres'
+      dialect: 'DB_DIALECT'
     }
   },
 };
